21f15acc80ddf8d00b1b5fc0e63968c47c5d38a1
Michael JasonSmith <mpj17@onlinegroups.net>
2012-09-20 10:12:16 +1000

    Code cleanup of the Manage Members form.

--- gs/group/member/manage/form.py
+++ gs/group/member/manage/form.py
@@ -1,44 +1,34 @@
 # coding=utf-8
-try:
-    from five.formlib.formbase import PageForm
-except ImportError:
-    from Products.Five.formlib.formbase import PageForm
-from zope.component import createObject
+from zope.cachedescriptors.property import Lazy
 from zope.formlib import form
 from Products.Five.browser.pagetemplatefile import ZopeTwoPageTemplateFile
-from gs.group.member.manage.manager import GSGroupMemberManager
+from gs.group.base.form import GroupForm
+from manager import GSGroupMemberManager
 
-class GSManageGroupMembersForm(PageForm):
+
+class GSManageGroupMembersForm(GroupForm):
     pageTemplateFileName = 'browser/templates/manage_members.pt'
     template = ZopeTwoPageTemplateFile(pageTemplateFileName)
-        
+
     def __init__(self, context, request):
-        PageForm.__init__(self, context, request)
-        self.context = context
-        self.request = request
-        self.siteInfo = createObject('groupserver.SiteInfo', context)
-        self.groupInfo = createObject('groupserver.GroupInfo', context)
+        GroupForm.__init__(self, context, request)
         self.groupName = self.groupInfo.name
         self.label = u'Manage the Members of %s' % self.groupName
-        self.showOnly = request.form.get('showOnly','')
-        self.page = request.form.get('page','1')
-        self.__memberManager = self.__membersInfo = None
-        self.__memberStatusActions = None
-        self.__form_fields = None
+        self.showOnly = request.form.get('showOnly', '')
+        self.page = request.form.get('page', '1')
 
-    @property
+    @Lazy
     def form_fields(self):
-        if self.__form_fields == None:
-            if not(self.memberManager.postingIsSpecial) and \
-              not(self.request.form.has_key('form.ptnCoachRemove')) and \
-              not(self.groupInfo.ptn_coach):
-                self.request.form['form.ptnCoachRemove'] = u'True'
-            if self.showOnly or not(self.memberManager.membersToShow):
-                self.__form_fields = self.memberManager.form_fields.omit('ptnCoachRemove')
-            else:
-                self.__form_fields = self.memberManager.form_fields
-        return self.__form_fields
-    
+        if (not(self.memberManager.postingIsSpecial)
+                and not('form.ptnCoachRemove' in self.request.form)
+                and not(self.groupInfo.ptn_coach)):
+            self.request.form['form.ptnCoachRemove'] = u'True'
+        if self.showOnly or not(self.memberManager.membersToShow):
+            retval = self.memberManager.form_fields.omit('ptnCoachRemove')
+        else:
+            retval = self.memberManager.form_fields
+        return retval
+
     def setUpWidgets(self, ignore_request=False, data={}):
         self.adapters = {}
         self.widgets = form.setUpWidgets(
@@ -48,7 +38,7 @@ class GSManageGroupMembersForm(PageForm):
         for widget in self.widgets:
             widget._displayItemForMissingValue = False
         assert self.widgets
-        
+
     @form.action(label=u'Change', failure='handle_change_action_failure')
     def handle_change(self, action, data):
         self.status = self.memberManager.make_changes(data)
@@ -61,12 +51,9 @@ class GSManageGroupMembersForm(PageForm):
             self.status = u'<p>There is an error:</p>'
         else:
             self.status = u'<p>There are errors:</p>'
-        
-    @property
-    def memberManager(self):
-        if self.__memberManager == None:
-            self.__memberManager = \
-               GSGroupMemberManager(self.groupInfo.groupObj, 
-                                    self.page, self.showOnly)
-        return self.__memberManager
 
+    @Lazy
+    def memberManager(self):
+        retval = GSGroupMemberManager(self.groupInfo.groupObj,
+                                        self.page, self.showOnly)
+        return retval
