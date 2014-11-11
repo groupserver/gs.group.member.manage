e4bb455ffcfe9bc03b615e134373c0ed7952dac8
Michael JasonSmith <mpj17@onlinegroups.net>
2014-11-10 16:27:24 -0800

    Further cleanup as I hunt the issue

--- gs/group/member/manage/statusformfields.py
+++ gs/group/member/manage/statusformfields.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-##############################################################################
+############################################################################
 #
 # Copyright © 2014 OnlineGroups.net and Contributors.
 # All Rights Reserved.
@@ -11,7 +11,7 @@
 # WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
 # FOR A PARTICULAR PURPOSE.
 #
-##############################################################################
+############################################################################
 from __future__ import absolute_import, unicode_literals
 from zope.cachedescriptors.property import Lazy
 from zope.component import adapts
@@ -32,7 +32,7 @@ class GSStatusFormFields(object):
 
     def __init__(self, status):
         assert IGSGroupMembershipStatus.providedBy(status), \
-          '%s is not a GSGroupMembershipStatus' % status
+            '%s is not a GSGroupMembershipStatus' % status
 
         self.status = status
         self.userInfo = status.userInfo
@@ -72,10 +72,10 @@ class GSStatusFormFields(object):
                 or self.status.isNormalMember or self.status.isPtnCoach
                 or self.status.isModerator)
             and not (self.status.isGroupAdmin or self.status.isModerated
-                or self.status.isOddlyConfigured)):
+                     or self.status.isOddlyConfigured)):
             t = 'Make %s a Group Administrator' % self.userInfo.name
             retval = Bool(__name__='%s-groupAdminAdd' % self.userInfo.id,
-                            title=t, description=t, required=False)
+                          title=t, description=t, required=False)
         # AM: Admins shouldn't be able to revoke the group-admin
         #   status of other admins of the same or higher rank.
         #elif self.status.isGroupAdmin and self.adminUserStatus.isSiteAdmin:
@@ -83,7 +83,7 @@ class GSStatusFormFields(object):
             t = 'Remove the Group Administrator privileges from %s' %\
                 self.userInfo.name
             retval = Bool(__name__='%s-groupAdminRemove' % self.userInfo.id,
-                            title=t, description=t, required=False)
+                          title=t, description=t, required=False)
         return retval
 
     @Lazy
@@ -91,16 +91,18 @@ class GSStatusFormFields(object):
         retval = False
         if (not(self.status.postingIsSpecial)
             and (self.status.isNormalMember or self.status.isSiteAdmin
-                or self.status.isGroupAdmin or self.status.isModerator)
+                 or self.status.isGroupAdmin or self.status.isModerator)
             and not (self.status.isPtnCoach or self.status.isModerated
-                    or self.status.isOddlyConfigured)):
-            ptnCoachTerm = SimpleTerm(True, True,
-                  'Make %s the Participation Coach' % self.userInfo.name)
+                     or self.status.isOddlyConfigured)):
+            ptnCoachTerm = SimpleTerm(
+                True, True,
+                'Make %s the Participation Coach' % self.userInfo.name)
             ptnCoachVocab = SimpleVocabulary([ptnCoachTerm])
             n = '%s-ptnCoach' % self.userInfo.id
-            retval = form.Fields(Choice(__name__=n, vocabulary=ptnCoachVocab,
+            retval = form.Fields(Choice(__name__=n,
+                                        vocabulary=ptnCoachVocab,
                                         required=False),
-                                custom_widget=radio_widget)
+                                 custom_widget=radio_widget)
         return retval
 
     @Lazy
@@ -108,15 +110,15 @@ class GSStatusFormFields(object):
         retval = False
         if (self.status.groupIsModerated
             and not (self.status.isModerator or self.status.isModerated
-                    or self.status.isInvited or self.status.isUnverified
-                    or self.status.isOddlyConfigured)):
+                     or self.status.isInvited or self.status.isUnverified
+                     or self.status.isOddlyConfigured)):
             t = 'Make %s a Moderator for this group' % self.userInfo.name
             retval = Bool(__name__='%s-moderatorAdd' % self.userInfo.id,
-                        title=t, description=t, required=False)
+                          title=t, description=t, required=False)
         elif self.status.groupIsModerated and self.status.isModerator:
             t = 'Revoke Moderator status from %s' % self.userInfo.name
             retval = Bool(__name__='%s-moderatorRemove' % self.userInfo.id,
-                        title=t, description=t, required=False)
+                          title=t, description=t, required=False)
         return retval
 
     @Lazy
@@ -125,11 +127,11 @@ class GSStatusFormFields(object):
         if self.status.groupIsModerated and self.status.isNormalMember:
             t = 'Start moderating messages from %s' % self.userInfo.name
             retval = Bool(__name__='%s-moderatedAdd' % self.userInfo.id,
-                            title=t, description=t, required=False)
+                          title=t, description=t, required=False)
         elif self.status.groupIsModerated and self.status.isModerated:
             t = 'Stop moderating messages from %s' % self.userInfo.name
             retval = Bool(__name__='%s-moderatedRemove' % self.userInfo.id,
-                    title=t, description=t, required=False)
+                          title=t, description=t, required=False)
         return retval
 
     @Lazy
@@ -138,15 +140,16 @@ class GSStatusFormFields(object):
         if (self.status.postingIsSpecial and
             (self.status.numPostingMembers < MAX_POSTING_MEMBERS)
             and not (self.status.isPostingMember or self.status.isUnverified
-                    or self.status.isOddlyConfigured)):
+                     or self.status.isOddlyConfigured)):
             t = 'Make %s a Posting Member' % self.userInfo.name
             retval = Bool(__name__='%s-postingMemberAdd' % self.userInfo.id,
-                            title=t, description=t, required=False)
+                          title=t, description=t, required=False)
         elif self.status.postingIsSpecial and self.status.isPostingMember:
             n = '%s-postingMemberRemove' % self.userInfo.id
             t = 'Revoke the Posting Member privileges from %s' % \
-                    self.userInfo.name
-            retval = Bool(__name__=n, title=t, description=t, required=False)
+                self.userInfo.name
+            retval = Bool(__name__=n, title=t, description=t,
+                          required=False)
         return retval
 
     @Lazy
@@ -158,10 +161,10 @@ class GSStatusFormFields(object):
         #  not(self.status.isGroupAdmin and \
         #      self.adminUserStatus.isGroupAdmin):
         if ((not self.status.isSiteAdmin) and (not self.status.isGroupAdmin)
-            and (not self.status.isInvited)):
+                and (not self.status.isInvited)):
             t = 'Remove %s from the group' % self.userInfo.name
             retval = Bool(__name__='%s-remove' % self.userInfo.id,
-                            title=t, description=t, required=False)
+                          title=t, description=t, required=False)
         return retval
 
     @Lazy
@@ -170,5 +173,5 @@ class GSStatusFormFields(object):
         if self.status.isInvited:
             t = 'Withdraw the invitation sent to %s' % self.userInfo.name
             retval = Bool(__name__='%s-withdraw' % self.userInfo.id,
-                            title=t, description=t, required=False)
+                          title=t, description=t, required=False)
         return retval
